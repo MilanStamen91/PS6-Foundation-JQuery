58f1446c81321c1189daa55259dae0a2c1862d85
Milan Stamenkovic <48022403+MilanStamen91@users.noreply.github.com>
2019-03-15 14:06:18 +0100

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+// shows and hides filtered items
+$(".filter-simple-button").click(function() {
+  var value = $(this).attr('data-filter');
+  if(value === "all") {
+    $('.filter-simple-item').show('1000');
+  } else {
+    $(".filter-simple-item").not('.'+value).hide('3000');
+    $('.filter-simple-item').filter('.'+value).show('3000');
+  }
+});
+
+// changes active class on filter buttons
+$('.filter-simple-button').click(function () {
+  $(this).siblings().removeClass('is-active');
+  $(this).addClass('is-active');
+});
+
+// Menu-toggle button
+
+//  $(document).ready(function() {
+//   $(".menu-icon").on("click", function() {
+//         $("nav ul").toggleClass("showing");
+//   });
+// });
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+  if($(window).scrollTop()) {
+        $('nav').addClass('black');
+  }
+  else {
+        $('nav').removeClass('black');
+  }
+})
+
